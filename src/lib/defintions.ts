eed4ff77a03770aa525d597b5c6cdb0124db16c7
IlijaRad <ilija.radivojevic237@gmail.com>
2025-08-04 19:57:14 +0200

    Type fix

--- src/lib/defintions.ts
+++ src/lib/defintions.ts
@@ -1,3 +1,7 @@
 export const AUTHENTICATION_COOKIE_NAME = "session";
 export const REGISTRATION_BANNER_COOKIE_NAME = "showRegistrationBanner";
 export const CHANGED_PASSWORD_BANNER_COOKIE_NAME = "changedPasswordBanner";
+
+export type SearchPrams = Promise<{
+  [key: string]: string | string[] | undefined;
+}>;
