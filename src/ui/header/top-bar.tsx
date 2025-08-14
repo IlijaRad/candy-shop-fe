7f3db98497065ab951ea249e0c0798e9ef027545
IlijaRad <ilija.radivojevic237@gmail.com>
2025-08-14 10:56:33 +0200

    Change top bar copy and padding

--- src/ui/header/top-bar.tsx
+++ src/ui/header/top-bar.tsx
@@ -7,32 +7,34 @@ export default async function TopBar() {
   const loggedIn = user !== null;
   return (
     <div className="bg-gray-900">
-      <div className="mx-auto flex h-10 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-        <div className="hidden lg:block lg:flex-1" aria-hidden="true" />
-        <div className="flex-1 text-center text-sm font-medium text-white lg:flex-none">
-          Besplatna dostava za porudžbine preko 5000RSD
-        </div>
-        {!loggedIn ? (
-          <nav className="hidden text-sm lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-            <Link
-              href="/register"
-              prefetch={false}
-              className="text-white transition-colors hover:text-gray-300"
-            >
-              Registruj se
-            </Link>
-            <div className="h-6 w-px bg-gray-600" aria-hidden="true" />
-            <Link
-              href="/login"
-              prefetch={false}
-              className="text-white transition-colors hover:text-gray-300"
-            >
-              Prijavi se
-            </Link>
-          </nav>
-        ) : (
+      <div className="px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto flex h-10 max-w-7xl items-center justify-between">
           <div className="hidden lg:block lg:flex-1" aria-hidden="true" />
-        )}
+          <div className="flex-1 text-center text-sm font-medium text-white lg:flex-none">
+            Besplatna dostava preko 5000RSD
+          </div>
+          {!loggedIn ? (
+            <nav className="hidden text-sm lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+              <Link
+                href="/register"
+                prefetch={false}
+                className="text-white transition-colors hover:text-gray-300"
+              >
+                Registruj se
+              </Link>
+              <div className="h-6 w-px bg-gray-600" aria-hidden="true" />
+              <Link
+                href="/login"
+                prefetch={false}
+                className="text-white transition-colors hover:text-gray-300"
+              >
+                Prijavi se
+              </Link>
+            </nav>
+          ) : (
+            <div className="hidden lg:block lg:flex-1" aria-hidden="true" />
+          )}
+        </div>
       </div>
     </div>
   );
